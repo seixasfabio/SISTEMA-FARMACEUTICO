b0af0575a89538597894cca573b46b3955e0ca9d
Fábio Alexandre Seixas Santos <145619817+seixasfabio@users.noreply.github.com>
2023-11-09 10:09:46 -0300

    Update Cadastro.js

--- Cadastro/Cadastro.js
+++ Cadastro/Cadastro.js
@@ -1,4 +1,3 @@
-const inputNome = document.querySelector("#NomeMedicamento");
 const inputPrincipio = document.querySelector("#PrincipioAtivo");
 const inputLote = document.querySelector("#Lote");
 const inputVencimento = document.querySelector("#Vencimento");
@@ -7,12 +6,13 @@ const bottonSubmit = document.querySelector("#Cadastrar");
 function verificar(event) {
     event.preventDefault();
     const cadastro = {
-        NomeMedicamento : inputNome.value,
         PrincipioAtivo : inputPrincipio.value,
         Lote : inputLote.value,
         Vencimento : inputVencimento.value,
     }
-    const Medicamento = [cadastro];
+    const Medicamento = Array();
+    Medicamento[document.querySelector("#NomeMedicamento").value] = cadastro;
+    Medicamento.push(cadastro);
     console.log(Medicamento);
 }
 bottonSubmit.onclick = verificar;
